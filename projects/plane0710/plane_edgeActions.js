b9f9fe791cc75280028833c4f0a3340ac5225dc1
DashaMaga <MagaDesign@Macs-Mac-mini.local>
2014-07-09 16:59:59 -0400

    plane0710

---
+++ projects/plane0710/plane_edgeActions.js
@@ -0,0 +1,14 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",2000,function(sym,e){sym.play("loop")});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'pl'
+(function(symbolName){})("pl");
+//Edge symbol end:'pl'
+})(jQuery,AdobeEdge,"EDGE-4255486");
